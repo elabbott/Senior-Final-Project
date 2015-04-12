73689711f93c43ed204c6febdb146bd0b6395fef
Jacob <jkb6292@gmail.com>
2015-04-12 15:15:20 -0400

    Added feedback option to Parent Users

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -1,5 +1,18 @@
 class FeedbacksController < InheritedResources::Base
 
+    def create
+    @feedback = Feedback.new(feedback_params)
+    respond_to do |format|
+      if @feedback.save
+        format.html { redirect_to homepages_url, notice: 'Feedback submitted!' }
+      else
+        format.html {render :new }
+        format.json { render json: @meal.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  
   private
 
     def feedback_params
