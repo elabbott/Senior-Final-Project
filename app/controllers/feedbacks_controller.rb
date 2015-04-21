81a57a37927a834c5097316a3f77ef82176aabc0
Jacob <jkb6292@gmail.com>
2015-04-21 16:51:08 -0400

    Feedbacks tab functionality added

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -3,6 +3,7 @@ class FeedbacksController < InheritedResources::Base
     def create
     @feedback = Feedback.new(feedback_params)
     @feedback.date_submitted = Date.today
+    @feedback.f_type = current_user.id
     respond_to do |format|
       if @feedback.save
         format.html { redirect_to homepages_url, notice: 'Feedback submitted!' }
